6d8011536fc06517fa1c2efed0ff0e231bac02a8
Andrey Glumov <gluman@yandex.ru>
2025-01-30 22:19:20 +0300

    fix errors. fix first time

--- task.js
+++ task.js
@@ -1,26 +1,45 @@
 const dropdownLists = document.getElementsByClassName('dropdown__list');
-const dropdownValues = document.getElementsByClassName('dropdown__value');
+const dropdownItem = document.getElementsByClassName('dropdown__link')
+const dropdownValue = document.getElementsByClassName('dropdown__value');
 
 
-for (let i = 0; i < dropdownValues.length; i++) {
-    dropdownValues[i].addEventListener('click', (event) => {
-    event.preventDefault();
-    
-        for (let y = 0; y < dropdownLists.length; y++) {
-            if (dropdownLists[y].classList.contains('dropdown__list_active')) {
-                dropdownLists[y].classList.remove('dropdown__list_active');
-            } else {
-                dropdownLists[y].classList.add('dropdown__list_active');
-            }
-        }
-    });
-}
+let active_lang
+console.log(dropdownItem)
+Array.from(dropdownItem).forEach(item => {
+    // dropdownItem.classList.add('dropdown__list_active')
+    item.addEventListener('click', event => {
+        event.preventDefault();
+        item.classList.add('dropdown__list_active')
+        active_lang = item[0].textContent()
+    })
+})
+console.log(dropdownValue)
 
-// $(document).ready(function() {
-//     $('a').click(function(event) {
-//         event.preventDefault();
-//         var dropdown = $(this).closest('.dropdown'); // Находим родительский контейнер dropdown
-//         var selectedItem = $(this).text(); // Получаем текст выбранной ссылки
-//         dropdown.find('.selected').text(selectedItem); // Обновляем текст в dropdown
+ {dropdownLists.addEventListener('click', val => {
+    val.classList.add('dropdown__list_active')
+    val.textContent() = active_lang
+})}
+
+// dropdownValue.classList.remove('dropdown__list_active');
+
+dropdownValue[0].addEventListener('click', value => {
+    Array.from(dropdownValue).forEach(value => {
+        value.classList.remove('dropdown__list_active');
+    })
+    value.classList.add('dropdown__list_active')
+})
+
+// for (let i = 0; i < dropdownValue.length; i++) {
+
+//     dropdownValue[i].addEventListener('click', (event) => {
+
+//         for (let y = 0; y < dropdownLists.length; y++) {
+//             if (dropdownLists[y].classList.contains('dropdown__list_active')) {
+//                 dropdownLists[y].classList.remove('dropdown__list_active');
+//             } else {
+//                 dropdownLists[y].classList.add('dropdown__list_active');
+//             }
+//         }
 //     });
-// });
+// }
+
