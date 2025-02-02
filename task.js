7e896f62f3fcd9b32ee4545b844371c489cf1e7f
Andrey Glumov <gluman@yandex.ru>
2025-02-02 16:48:24 +0300

    fix errors, final commit today

--- task.js
+++ task.js
@@ -1,45 +1,23 @@
-const dropdownLists = document.getElementsByClassName('dropdown__list');
-const dropdownItem = document.getElementsByClassName('dropdown__link')
-const dropdownValue = document.getElementsByClassName('dropdown__value');
+const dropdownLinks = Array.from(document.getElementsByClassName('dropdown__link'));
+const dropdownValue = Array.from(document.getElementsByClassName('dropdown__value'));
+const dropdownLists = Array.from(document.getElementsByClassName('dropdown__list'));
 
+function activate_list() {
+    dropdownLists.forEach(list => {
+        list.classList.toggle('dropdown__list_active')
+    })
+}
 
-let active_lang
-console.log(dropdownItem)
-Array.from(dropdownItem).forEach(item => {
-    // dropdownItem.classList.add('dropdown__list_active')
-    item.addEventListener('click', event => {
+dropdownValue.forEach(value => {
+    value.addEventListener('click', value => {
+        activate_list()
+    })
+})
+
+dropdownLinks.forEach(link => {
+    link.addEventListener('click', event => {
         event.preventDefault();
-        item.classList.add('dropdown__list_active')
-        active_lang = item[0].textContent()
+        dropdownValue[0].textContent = link.textContent
+        activate_list()
     })
 })
-console.log(dropdownValue)
-
- {dropdownLists.addEventListener('click', val => {
-    val.classList.add('dropdown__list_active')
-    val.textContent() = active_lang
-})}
-
-// dropdownValue.classList.remove('dropdown__list_active');
-
-dropdownValue[0].addEventListener('click', value => {
-    Array.from(dropdownValue).forEach(value => {
-        value.classList.remove('dropdown__list_active');
-    })
-    value.classList.add('dropdown__list_active')
-})
-
-// for (let i = 0; i < dropdownValue.length; i++) {
-
-//     dropdownValue[i].addEventListener('click', (event) => {
-
-//         for (let y = 0; y < dropdownLists.length; y++) {
-//             if (dropdownLists[y].classList.contains('dropdown__list_active')) {
-//                 dropdownLists[y].classList.remove('dropdown__list_active');
-//             } else {
-//                 dropdownLists[y].classList.add('dropdown__list_active');
-//             }
-//         }
-//     });
-// }
-
